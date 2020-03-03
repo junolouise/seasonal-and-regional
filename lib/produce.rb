4b7218721bf048187e47688862801f31f41307d2
ria patel <ria.amy.patel@gmail.com>
2020-03-03 20:44:13 +0000

    refactored produce methods using extracted database logic

--- lib/produce.rb
+++ lib/produce.rb
@@ -1,15 +1,13 @@
-require 'pg'
+require './lib/database_connection'
 
 class Produce
   def self.all
-    connection = PG.connect(dbname: 'hangry_development')
-    result = connection.exec('SELECT * FROM produce')
+    result = DatabaseConnection.query('SELECT * FROM produce')
     result.map { |item| item['name']}
   end
 
   def self.in_season
-    connection = PG.connect(dbname: 'hangry_development')
-    result = connection.exec('SELECT * FROM produce WHERE current_date <@ daterange1 OR current_date <@ daterange2;')
+    result = DatabaseConnection.query('SELECT * FROM produce WHERE current_date <@ daterange1 OR current_date <@ daterange2')
     result.map { |item| item['name'] }
   end
 end
