3dd4360377ef78c7376bf53c143d5b35ce750177
Katie Rodgers <katierodgers95@gmail.com>
2020-03-03 16:42:37 +0000

    setup: jest

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>
+    <p>{{ copyright }}</p>
+    <p>{{ credits }}</p>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      copyright: "Copyright 2020 Hangry",
+      credits: "Data retrieved from Eaternity API"
+    };
+  }
+};
+</script>
+<style scoped>
+footer {
+  background: #222;
+  padding: 6px;
+}
+p {
+  color: lightseagreen;
+  text-align: center;
+}
+</style>
---
+++ tests/unit/Footer.test.js
@@ -0,0 +1,11 @@
+import { shallowMount } from '@vue/test-utils'
+import Footer from '@/components/Footer.vue'
+describe('Footer.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(Footer, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
