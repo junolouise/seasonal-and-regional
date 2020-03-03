8e5a8307fa24c0137f0fb311c3c34fe05b759fae
Katie Rodgers <katierodgers95@gmail.com>
2020-03-03 16:50:12 +0000

    feat: added components

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Seasonal Regional"
+    };
+  }
+};
+</script>
+
+<style scoped>
+header {
+  background: lightseagreen;
+  padding: 10px;
+}
+h1 {
+  color: #222;
+  text-align: center;
+}
+</style>
---
+++ src/components/Ingredients.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="ingredients">
+    <ul>
+      <li
+        v-for="ingredient in ingredients"
+        v-on:click="ingredient.show = !ingredient.show"
+      >
+        <h2>{{ ingredient.name }}</h2>
+        <h3 v-show="ingredient.show">{{ ingredient.season }}</h3>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      ingredients: [
+        {
+          name: "Apple",
+          season: "Jan, Feb,... Sep, Oct, Nov, Dec",
+          show: false
+        },
+        { name: "Asparagus", season: "May, Jun, Jul, Aug, Sep", show: false },
+        {
+          name: "Beef",
+          season: "Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec",
+          show: false
+        },
+        {
+          name: "Beetroot",
+          season: "Jan, ... Jul, Aug, Sep, Oct, Nov, Dec",
+          show: false
+        },
+        {
+          name: "Onion",
+          season: "Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec",
+          show: false
+        },
+        {
+          name: "Mussels",
+          season: "Jan, Feb, Mar, Oct, Nov, Dec",
+          show: false
+        },
+        { name: "Watercress", season: "May, Jun, Jul, Aug, Sep", show: false }
+      ]
+    };
+  }
+};
+</script>
+<style scoped>
+#ingredients {
+  width: 100%;
+  max-width: 1200px;
+  margin: 40px auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  flex-grow: 1;
+  flex-basis: 300px;
+  text-align: center;
+  padding: 30px;
+  border: 1px solid #222;
+  margin: 10px;
+  max-width: 300px;
+}
+</style>
