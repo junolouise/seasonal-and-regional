bb5743f635aa39f4711dc79a797d4a2f49298575
ria patel <ria.amy.patel@gmail.com>
2020-03-03 20:37:02 +0000

    db connection is persistent

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -1,5 +1,9 @@
 class DatabaseConnection 
   def self.setup(dbname)
-    PG.connect(dbname: dbname)
+    @connection = PG.connect(dbname: dbname)
+  end
+
+  def self.connection
+    @connection
   end
 end
--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -7,4 +7,11 @@ describe DatabaseConnection do
       DatabaseConnection.setup('hangry_development')
     end
   end
+
+  describe '.connection' do
+    it 'the connection is persistent' do
+      connection = DatabaseConnection.setup('hangry_development')
+      expect(DatabaseConnection.connection).to eq connection
+    end
+  end
 end
