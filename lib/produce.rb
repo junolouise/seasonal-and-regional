ed6bcb3ca904805294b2524c634fa601d9c11b88
ria patel <ria.amy.patel@gmail.com>
2020-03-03 20:30:57 +0000

    .in_season returns a list of produce that are currently in season

--- lib/produce.rb
+++ lib/produce.rb
@@ -6,4 +6,10 @@ class Produce
     result = connection.exec('SELECT * FROM produce')
     result.map { |item| item['name']}
   end
+
+  def self.in_season
+    connection = PG.connect(dbname: 'hangry_development')
+    result = connection.exec('SELECT * FROM produce WHERE current_date <@ daterange1 OR current_date <@ daterange2;')
+    result.map { |item| item['name'] }
+  end
 end
--- spec/produce_spec.rb
+++ spec/produce_spec.rb
@@ -9,4 +9,11 @@ describe Produce do
       expect(produce).to include 'leek'
     end
   end
+
+  describe '.in_season' do
+    it 'returns a list of in season produce' do
+      produce = Produce.in_season
+      expect(produce).to include 'halibut'
+    end
+  end
 end
