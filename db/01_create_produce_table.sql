713475fd17e76f00e833f36b7f2fdc2a1f0126ab
ria patel <ria.amy.patel@gmail.com>
2020-03-04 11:33:22 +0000

    creating table populated with produce and seasonality

--- db/01_create_produce_table.sql
+++ db/01_create_produce_table.sql
@@ -8,7 +8,7 @@
 -- -------------------------------------------------------------
 
 
-DROP TABLE IF EXISTS "public"."produce";
+DROP TABLE IF EXISTS "public"."produce"; 
 -- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
 -- Sequence and defined type
