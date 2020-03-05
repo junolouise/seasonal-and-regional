2363118a29333f5522ef0609d0a0c54241f84c3a
Katie Rodgers <katierodgers95@gmail.com>
2020-03-05 18:26:03 +0000

    fix: adding in dependency

---
+++ config.js
@@ -0,0 +1,11 @@
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyCeEXdHCml2I7ieN1epGgyHgnmv40dCWYo",
+  authDomain: "seasonal-regional.firebaseapp.com",
+  databaseURL: "https://seasonal-regional.firebaseio.com",
+  projectId: "seasonal-regional",
+  storageBucket: "seasonal-regional.appspot.com",
+  messagingSenderId: "96103047575",
+}
+// Exports
+module.exports = firebaseConfig;
