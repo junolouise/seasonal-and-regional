9a228d8d805c2291c437d4a4c5e1434c919b8916
Marcellofabbri <marcellogfabbri@gmail.com>
2020-03-05 16:59:43 +0000

    passing params to the API

--- src/components/Menus.vue
+++ src/components/Menus.vue
@@ -4,7 +4,7 @@
 		<ul>
 			<li v-for="(menu, index) in menus" :key="index">
 				<h2>{{ menu.recipe.label }}</h2>
-				<h3>{{ menu.recipe.calories }}</h3>
+				<h3>{{ menu.recipe.url }}</h3>
 			</li>
 		</ul>
 		{{menus}}
@@ -26,7 +26,7 @@ export default {
 	// 		});
 	// 	},
 	// },
-	created() {this.$http.get('https://api.edamam.com/search?q=chicken&app_id=649a31c0&app_key=1cd135a5aa58498a06ad42aa6798aeeb').then(function(data){ this.menus = data.body.hits });}
+	created() {this.$http.get('https://api.edamam.com/search?q=' + this.$route.params.name + '&app_id=649a31c0&app_key=1cd135a5aa58498a06ad42aa6798aeeb').then(function(data){ this.menus = data.body.hits });}
 };
 </script>
 <style scoped>
