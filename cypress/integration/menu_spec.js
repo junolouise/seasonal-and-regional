8f3af47b7d6acb85bf72b98869c223f919858b47
Katie Rodgers <katierodgers95@gmail.com>
2020-03-06 13:33:20 +0000

    test: menu tests added

--- cypress/integration/menu_spec.js
+++ cypress/integration/menu_spec.js
@@ -6,4 +6,20 @@ describe('Menus', function() {
 
     cy.url().should('include', '/menus')
   })
+
+  it('Displays recipes for the selected ingredient', function() {
+    cy.visit('https://seasonal-regional.herokuapp.com/')
+
+    cy.contains('beef').click()
+
+    cy.contains('beef')
+  })
+
+  it('Routes to correct url from selected ingredient', function() {
+    cy.visit('https://seasonal-regional.herokuapp.com/')
+
+    cy.contains('beef').click()
+
+    cy.url().should('include', 'beef')
+  })
 })
