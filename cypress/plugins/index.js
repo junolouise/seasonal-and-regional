c5ea7eb65e70b92801c38cd5f17cf8af9ba24563
Marcellofabbri <marcellogfabbri@gmail.com>
2020-03-09 17:09:15 +0000

    halfway through implementing coverage

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,6 +16,7 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
+  on('task', require('@cypress/code-coverage/task'))
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
