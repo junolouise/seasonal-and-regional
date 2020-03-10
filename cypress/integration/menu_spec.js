993cf49cbf7165c8846daf4c4ef2973c053c0bcc
Marcellofabbri <marcellogfabbri@gmail.com>
2020-03-10 16:39:31 +0000

    more tests

--- cypress/integration/menu_spec.js
+++ cypress/integration/menu_spec.js
@@ -22,10 +22,33 @@ describe('Recipes', function() {
 		cy.contains('Apple');
 	});
 
-	it('Displays  the details of a recipe with weights for the selected recipe', function() {
+	it('Displays the details of a recipe with weights for the selected recipe', function() {
 		cy.visit('https://seasonal-regional.herokuapp.com/recipes/beef');
 		cy.contains('Beef').click({ force: true });
 
 		cy.contains('pepper');
 	});
+
+	it('Displays all ingredients when the Show-All button is clicked', function() {
+		cy.visit('https://seasonal-regional.herokuapp.com/');
+		cy.contains('Show all').click();
+		cy.contains('apricot')
+		cy.contains('mackerel')
+	})
+
+	it('Recipe ingredients appear when a recipe is clicked on', function() {
+		cy.visit('https://seasonal-regional.herokuapp.com/');
+		cy.contains('January').click();
+		cy.get('h2').contains('apple').click();
+		cy.get('h2').contains('Apple Slaw').click();
+		cy.contains('tablespoon fresh lemon juice')
+	})
+
+	it('Individual recipe page has link to original source', function() {
+		cy.visit('https://seasonal-regional.herokuapp.com/');
+		cy.contains('January').click();
+		cy.get('h2').contains('apple').click();
+		cy.get('h2').contains('Apple Slaw').click();
+		cy.contains('View source Recipe')
+	})
 });
